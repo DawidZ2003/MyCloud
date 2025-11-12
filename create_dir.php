dc4a048b7f8987fdbc8dcdf06fbc35c1b729ec6f
DawidZ2003 <mamba75210@interia.pl>
2025-11-12 12:35:38 +0100

    Update create_dir.php

--- create_dir.php
+++ create_dir.php
@@ -1,14 +1,20 @@
 <?php
+// Start sesji
 session_start();
-if (!isset($_SESSION['loggedin'])) { header('Location: logowanie.php'); exit(); }
+// Sprawdzenie logowania
+if (!isset($_SESSION['loggedin'])) { 
+    header('Location: logowanie.php'); 
+    exit(); 
+}
+// Katalog domowy użytkownika
 $homeDir = $_SESSION['home_dir'];
-
+// Tworzenie nowego katalogu
 if (!empty($_POST['new_dir'])) {
-    $newDir = basename($_POST['new_dir']);
+    $newDir = basename($_POST['new_dir']); // nazwa katalogu bez ścieżki
     $path = $homeDir . DIRECTORY_SEPARATOR . $newDir;
     if (!is_dir($path)) mkdir($path, 0777, true);
 }
-
+// Powrót na stronę główną
 header('Location: index.php');
 exit();
 ?>
